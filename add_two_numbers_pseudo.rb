49eb201ab440f4f60813a3d78402c6592a77cd3b
gtsofa <jlsnyule@yahoo.com>
2019-08-28 12:31:48 +0300

    Array and Hashes How to work with array and hashes Implement iterations on arrays & hashes

---
+++ add_two_numbers_pseudo.rb
@@ -0,0 +1,25 @@
+# Requirements
+# - Get two integer values from a user and output the sum of those numbers.
+# pseudo code
+# prompt a user for a number 
+p "enter a number"
+num1 = gets
+# Add conditional statement to allow legal values
+# if input is not a number
+if !num1.is_a?(Integer)
+# print 'input is not a number'
+    puts "#{num1} is not a number"
+    # prompt user for new number 
+    puts "enter a number"
+# endif 
+# set num1 to user_input
+# num1 = gets
+# prompt user for another number
+# if input is not a number
+#     print 'input is not a number'
+#     prompt user for new number 
+# endif 
+# set num2 to user_input
+# set sum to num1 + num2
+# print sum 
+    end
