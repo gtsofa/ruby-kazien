c12b8e4530cb243a3df3049a94a45f167c33bca4
gtsofa <jlsnyule@yahoo.com>
2019-08-30 23:58:11 +0300

    print the contact list

--- contact_list.rb
+++ contact_list.rb
@@ -26,8 +26,19 @@ contact_list = []
 answer = ""
 while answer != "n"
     contact_list.push(add_contact())
-    answer = ask("Add another? (y/n)")
+    answer = ask("Add another contact? (y/n)")
 end
 
+puts "--"
+contact_list.each do |contact|
+    puts "Name: #{contact["name"]}"
+    if contact["phone_number"].size > 0
+        contact["phone_number"].each do |phone_number|
+            puts "Phone: #{phone_number} "
+        end
+    end
+    puts "-----\n"
+end
+    
 # answer = ask("What is your name?")
 # puts answer
