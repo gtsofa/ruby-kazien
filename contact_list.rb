a18b19245062b08982e2fd518f96c43132b66f41
gtsofa <jlsnyule@yahoo.com>
2019-08-30 23:34:07 +0300

    Add a contact

--- contact_list.rb
+++ contact_list.rb
@@ -1,4 +1,3 @@
-contact_list = []
 
 def ask(question, kind="string")
     print question + " "
@@ -7,5 +6,28 @@ def ask(question, kind="string")
     return answer
 end
 
-answer = ask("What is your name?")
-puts answer
+def add_contact
+    # create a hash of contact
+    contact = {"name" => "", "phone_number" => []}
+    contact["name"] = ask("What is the person's name?")
+    answer = ""
+    while answer != "n"
+        answer = ask("Would you like to add a phone number? (y/n)")
+        if answer == "y"
+            phone = ask("Enter a phone number:")
+            contact["phone_number"].push(phone)
+        end
+    end
+    return contact
+    
+end
+
+contact_list = []
+answer = ""
+while answer != "n"
+    contact_list.push(add_contact())
+    answer = ask("Add another? (y/n)")
+end
+
+# answer = ask("What is your name?")
+# puts answer
