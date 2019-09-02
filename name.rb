d9594db9fce7d15b3ddfb717b096494c55bcc3c7
gtsofa <jlsnyule@yahoo.com>
2019-09-02 21:58:52 +0300

    convert to string

--- name.rb
+++ name.rb
@@ -1,5 +1,5 @@
 class Name
-    # attr_accessor :title, :first_name, :middle_name, :last_name
+    attr_accessor :title, :first_name, :middle_name, :last_name
     # attr_reader :first_and_middle_name
   
     def initialize(title, first_name, middle_name, last_name)
@@ -10,17 +10,19 @@ class Name
     end
   
     def full_name
-      @first_and_middle_name = @first_name + " " + @middle_name
-  
-      @first_and_middle_name + " " + @last_name
+      first_name + " " + middle_name + " " + last_name
     end
   
-    def full_name_with_title
-      @title + " " + full_name()
+    # def full_name_with_title
+    #   @title + " " + full_name()
+    # end
+
+    def to_s
+      full_name
     end
   
   
   end
   
   name = Name.new("Mr.", "Jason", "", "Seifer")
-  puts name.full_name_with_title
+  puts name.inspect
