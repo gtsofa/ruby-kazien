b354e02f8af3c2b0ec8a3a47985dd32f5ef9c150
gtsofa <jlsnyule@yahoo.com>
2019-09-06 01:21:05 +0300

    format contact

--- AddressBook/contact.rb
+++ AddressBook/contact.rb
@@ -5,6 +5,20 @@ class Contact
         @first_name
     end
 
+    def last_first
+        last_first = last_name
+        last_first += ", "
+        last_first += first_name
+        if !middle_name.nil?
+            last_first += " "
+            last_first += middle_name.slice(0, 1)
+            last_first += "."
+        end
+        last_first
+
+
+    end
+
     def middle_name
         @middle_name
     end
@@ -23,16 +37,42 @@ class Contact
         full_name += last_name
         full_name
     end
+
+    def to_s(format = "full_name")
+        case format
+        when 'full_name'
+            full_name
+        when 'last_first'
+            last_first
+        when 'first_name'
+            first_name
+        when 'first'
+            first_name
+        when 'last'
+            last_name
+        else
+            last_first
+        end
+    end
 end
 
 # testing
 tsofa = Contact.new
 tsofa.first_name = "Tsofa"
 tsofa.last_name = "Nyule"
-puts tsofa.full_name
+puts tsofa.to_s
+puts tsofa.to_s('last_first')
+puts tsofa.to_s('full_name')
+# puts tsofa.full_name
+# puts tsofa.last_first
+
 
 sarah = Contact.new
 sarah.first_name = "Sarah"
 sarah.middle_name = "Amore"
 sarah.last_name = "Mio"
-puts sarah.full_name
+puts sarah.to_s
+puts sarah.to_s('last_first')
+puts sarah.to_s('full_name')
+# puts sarah.full_name
+# puts sarah.last_first
