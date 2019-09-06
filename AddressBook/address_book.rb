ba904fa41a7cdf456368271864dd2e10d118d7c4
gtsofa <jlsnyule@yahoo.com>
2019-09-06 21:48:55 +0300

    Search contact by phone number

--- AddressBook/address_book.rb
+++ AddressBook/address_book.rb
@@ -6,6 +6,18 @@ class AddressBook
         @contacts = []
     end
 
+    def print_results(search, results)
+        puts search
+        results.each do |contact|
+            puts contact.to_s('full_name')
+            contact.print_phone_numbers
+            contact.print_addresses
+            puts "\n"
+        end
+        # print_results("Name search results (#{search})", results)
+
+    end
+
     def fint_by_name(name)
         results = []
         search = name.downcase
@@ -15,21 +27,31 @@ class AddressBook
                 results.push(contact)
             end
         end
-        puts "Name search results (#{search})"
-        results.each do |result|
-            puts result.to_s('full_name')
-            result.print_phone_numbers
-            result.print_addresses
-        end
+        print_results("Name search results (#{search})", results)
 
     end
 
-    def print_contact_list
-        puts "Contact List"
+    def find_by_phone_number(number)
+        results = []
+        search = number.gsub("-", "")
         contacts.each do |contact|
-            puts contact.to_s('last_first')
+            contact.phone_numbers.each do |phone_number|
+                if phone_number.number.gsub("-", "").include?(search)
+                    results.push(contact) unless results.include?(contact)
+                end
+            end
         end
+
+        print_results("Phone search results (#{search})", results)
+        
     end
+
+    # def print_contact_list
+    #     puts "Contact List"
+    #     contacts.each do |contact|
+    #         puts contact.to_s('last_first')
+    #     end
+    # end
 end
 
 address_book = AddressBook.new
@@ -45,7 +67,7 @@ tsofa.add_address("Home", "123 Main St.", "", "Portland", "OR", "12345")
 maestro = Contact.new
 maestro.first_name = "Maestro"
 maestro.last_name = "Julio"
-maestro.add_phone_number("Acasa", "123-456-7890")
+maestro.add_phone_number("Acasa", "222-456-7890")
 maestro.add_phone_number("Lavoro", "456-7890-888")
 maestro.add_phone_number("Cellulare", "333-908-765")
 maestro.add_address("Home", "123 Main St.", "", "Portland", "OR", "12345")
@@ -53,4 +75,5 @@ maestro.add_address("Home", "123 Main St.", "", "Portland", "OR", "12345")
 address_book.contacts.push(tsofa)
 address_book.contacts.push(maestro)
 # address_book.print_contact_list
-address_book.fint_by_name('n')
+# address_book.fint_by_name('n')
+address_book.find_by_phone_number("8")
