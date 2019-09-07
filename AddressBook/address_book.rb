cffdd431dc39114aee7faa59b1318e676619887a
gtsofa <jlsnyule@yahoo.com>
2019-09-07 22:50:11 +0300

    Searching by address

--- AddressBook/address_book.rb
+++ AddressBook/address_book.rb
@@ -46,12 +46,27 @@ class AddressBook
         
     end
 
-    # def print_contact_list
-    #     puts "Contact List"
-    #     contacts.each do |contact|
-    #         puts contact.to_s('last_first')
-    #     end
-    # end
+    def find_by_address(query)
+        results = []
+        search = query.downcase
+        contacts.each do |contact|
+            contact.addresses.each do |address|
+                if address.to_s('long').downcase.include?(search)
+                    results.push(contact) unless results.include?(contact)
+                end
+            end
+        end
+
+        print_results("Address search results (#{search})", results)
+
+    end
+
+    def print_contact_list
+        puts "Contact List"
+        contacts.each do |contact|
+            puts contact.to_s('last_first')
+        end
+    end
 end
 
 address_book = AddressBook.new
@@ -70,10 +85,11 @@ maestro.last_name = "Julio"
 maestro.add_phone_number("Acasa", "222-456-7890")
 maestro.add_phone_number("Lavoro", "456-7890-888")
 maestro.add_phone_number("Cellulare", "333-908-765")
-maestro.add_address("Home", "123 Main St.", "", "Portland", "OR", "12345")
+maestro.add_address("Home", "222 Two Lane", "", "Portland", "OR", "12345")
 
 address_book.contacts.push(tsofa)
 address_book.contacts.push(maestro)
 # address_book.print_contact_list
 # address_book.fint_by_name('n')
 address_book.find_by_phone_number("8")
+address_book.find_by_address("two")
