caa169f1d0833ec9cefa8bdfcbbc4ea188a2578c
gtsofa <jlsnyule@yahoo.com>
2019-11-07 23:22:54 +0300

    add reverse string pseudocode

---
+++ reverse_string.rb
@@ -0,0 +1,18 @@
+# pseudocode
+# Reverse a string 
+# create an empty string
+# pick the last letter from the given string and add it to the new_string 
+# repeat step 2 until the given string is empty
+# display the new_string
+
+
+nome = "abel" #leba
+
+r_string = ''
+
+a = nome[0] + r_string
+b = nome[1] + r_string
+c = nome[2] + r_string
+d = nome[3] + r_string
+
+puts "#{d} #{c} #{b} #{a}"
